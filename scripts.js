ec79d5fea70474c92bd12fcf8d996d2f1603eebb
samuel reyes <SAMCRO@samuels-MacBook-Pro.local>
2015-11-24 14:27:50 -0500

    updated

--- scripts.js
+++ scripts.js
@@ -1,1 +1,41 @@
 console.log('...Whats UP')
+
+
+<style>
+body{
+    color: red;
+    /* This is a single-line comment */
+    text-align: center;
+}
+</style>
+
+
+
+
+
+
+
+/*<h2>Do you know "the Rock"</h2>
+
+//var name = "Dwayne Johnson";
+var question1 = "What is Rocks name" "?";
+
+//console.log(question1);
+
+<p>What year was "the Rocks" debut?</p>
+
+<p>Whats "The Rock" real name?</p>
+
+<p>What was "The Rock" first wrestler name</p>
+
+<p>Whats "The Rock" favorite instrument?</p>
+
+<p>What does he do to hes challengers?</p>
+
+<p>Whats the Rocks signature finish move?</p>
+
+<p>How old is "The Rock"?</p>
+
+<p>Who is "The Rock" biggest rival?</p>
+
+<p>Who is the Rocks farther ?</p>*/
