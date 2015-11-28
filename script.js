c8104688b76a4ec29b9812d625a08f32b8d2cb4a
samuel reyes <SAMCRO@samuels-MBP.home>
2015-11-28 15:41:41 -0500

    seconddraft

---
+++ script.js
@@ -0,0 +1,42 @@
+//Game state
+//the Rock
+//Sings, run, dance, smackdown, insults
+//humor sayings, facial expressions
+// loading static pg : how will the page welome the player
+
+
+/*var name = "Jeff";
+var question1 = "What is your name " + name + "?";
+
+console.log(question1);
+*/
+
+
+
+
+
+//make a array list ?
+
+// steps
+// 1- ask questions , 
+// 2-Whats the Rocks real name ?
+// How old is the rock
+// what year did he become champion / who did he beat ?
+
+// whats hes farthers wrestling name
+// what the name of the rocks  signature move
+// does the rock dance ?
+//Does the rock play a instrument?
+
+
+// click option : state player name
+
+// firts question ; multiple choice : find questions and attach right answer
+
+// click on answer the correct: prompt reward : keep a scroeboard
+
+// click on wrong answer : prompt wrong answer :
+// scroeboard with progression
+
+// loop until one runs out of questions :
+// win option and loosing option : winner gets belt / loser gets rock bottom / or insults
--- scripts.js
+++ scripts.js
@@ -1,18 +1,138 @@
 console.log('...Whats UP')
 
 
-<style>
-body{
-    color: red;
-    /* This is a single-line comment */
-    text-align: center;
+
+var questions = [
+  "Is the rock 6'5 inches tall",
+  "Is the rock real name Dwayne Johnson",
+  "Is the Rock the current wwe champion",
+  "Is the rock signature finish move the peoples elbow",
+  "Did the rock invent the rock bottom finesher",
+  "Is brett heart the rocks biggest rival",
+  "Was the rock's farther a wrestler",
+  "Did you ever smell what the rock was cooking",  "Are the drums the rock's favorite instrument",
+  "Does the rock Sings",
+  "Was the rocks firt debut in 1996 survivor series"
+];
+
+var answers = [
+  "yes",
+  "yes",
+  "no",
+  "yes",
+  "no",
+  "no",
+  "yes",
+  "no",
+  "no",
+  "yes",
+  "yes",
+];
+
+var answer;
+
+
+function getMeAQuestionbro() {
+  var rand = Math.floor(Math.random() * questions.length);
+  var question = questions[rand]
+  console.log(question);
+  // console.log(answer);
+  return question
 }
-</style>
+
+function getAnswer(question){
+  var idx = questions.indexOf(question);
+  console.log(idx);
+  return answers[idx]
+}
+
+
+function renderQuestion() {
+  var question = getMeAQuestionbro();
+  $('#question').html( question );
+  var answer = getAnswer(question);
+  console.log(answer);
+}
+
+
+
+function setupListeners() {
+ $('button#yes').on('click', function(){
+   renderQuestion();
+ });
+
+}
+
+renderQuestion();
+setupListeners();
+
+var pictures = [
+"http://dailywrestlingnews.com/wp-content/uploads/2014/10/Rock-SD-moments-gifs-001.gif",
+"https://media.giphy.com/media/4g6xgP7FXjy12/giphy.gif", "https://media.giphy.com/media/kMSyCATSq9SEw/giphy.gif",
+"https://media.giphy.com/media/V53jWRdPWwO0U/giphy.gif", "https://media3.giphy.com/media/Af6dsiJBh21qg/200.gif",
+"https://media2.giphy.com/media/zainSSjEMwOHe/200.gif", "https://media3.giphy.com/media/A9AlCLULGEVpe/200.gif",
+"https://media1.giphy.com/media/127h8dMHnk5H5C/200.gif", "https://media1.giphy.com/media/vAEuGaCUzM676/200.gif",
+"https://media1.giphy.com/media/5BdqG8zyAzr7W/200.gif", "https://media2.giphy.com/media/bSYfwd2fBTWmc/200.gif",
+]
+
+var randomNumber = function(){
+  return Math.floor(Math.random() *11);
+}
+
+//var checkAnswer(question, answer){
+  // if answer is right
+  //if (answer === question)
+  //return true;
+  // else
+  // if answer is wrong
+  //return false;
+//}
+
+$(document).ready(function(){
+  $("#yes").on("click", function(){
+    var question = $("#question").text();
+    var idx = questions.indexOf(question);
+    var answer = answers[idx];
+
+    if (answer === "yes"){
+      $("#results").html("your Right" + "")
+    } else {
+      console.log('incorrect');
+      $("#results").html("your Wrong Jabroni" + "")
+    }
+
+    $("#picId").attr("src", pictures[ randomNumber() ]);
+  });
+
+  $("#no").on("click", function(){
+    var question = $("#question").text();
+    var idx = questions.indexOf(question);
+    var answer = answers[idx];
+
+    if (answer === "no"){
+      $("#results").html("your Right" + "")
+    } else {
+      console.log('incorrect');
+      $("#results").html("your Wrong Jabroni" + "")
+    }
+
+    $("#picId").attr("src", pictures[ randomNumber() ]);
+  });
+
+
+})
 
 
 
 
 
+
+
+
+
+
+
+// function(question, answer)on.click
 
 
 /*<h2>Do you know "the Rock"</h2>
